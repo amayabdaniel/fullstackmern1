1cbe5d7f1b3b090aa346514a9934050ef3870001
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-03-01 11:30:16 -0500

    [UPDATE] postRouter

--- server/routes/postsRouter.js
+++ server/routes/postsRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getPosts } from '../controllers/postsController.js'
+import { getPosts, createPost } from '../controllers/postsController.js'
 
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.post('/', createPost);
 
 
 export default router;
